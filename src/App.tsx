3e3d9182970bf5ea60217cf723f8e772640a32f5
jarydo <jarydnoahdiamond@gmail.com>
2025-07-30 18:41:25 -0700

    delay release

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,7 @@ const channels: ChannelType[] = [
     date: "2025-07-20",
     img: "mii_maker.png",
   },
-  { name: "Project 6", repo: "", date: "2025-07-29" },
+  { name: "Project 6", repo: "", date: "2025-07-31" },
   { name: "Project 7", repo: "", date: "2025-08-03" },
   { name: "Project 8", repo: "", date: "2025-08-10" },
   { name: "Project 9", repo: "", date: "2025-08-17" },
